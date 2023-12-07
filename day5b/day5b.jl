4850e6d4ecee6a2422f28464bb404c6796d58617
gmd01bit <gmd01bit@gmail.com>
2023-12-07 14:48:56 +0200

    :zap: Improve Day 5b in Julia

--- day5b/day5b.jl
+++ day5b/day5b.jl
@@ -5,53 +5,50 @@ include("../aoc.jl")
 within(r::UnitRange{Int}, lim::Int) = (r.stop >= 0) & (r.start < lim)
 
 inrange(r::UnitRange{Int}, lim::Int) =
-	[r.start:-1, max(r.start, 0):min(r.stop, lim-1), lim:r.stop]
+	UnitRange{Int}[r.start:-1, max(r.start, 0):min(r.stop, lim - 1), lim:r.stop]
 
 add(r::UnitRange{Int}, d::Int) = r .+ d
 
 function recurse(start::Vector{UnitRange{Int}}, maps::Vector{Matrix{Int}})
-	for map = maps
-		dest, src, sets = map[1, :], map[2, :], map[3, :]
-		iter = copy(start)
-		start = UnitRange{Int}[]
-		for _ = 1:4
-			!isempty(iter) || break
-			offset = broadcast((s, c) -> s .- c, permutedims(iter), src)
-			closed = inrange.(offset, sets)
-			index = findall(within.(offset, sets))
-			if isempty(index)
-				continue
-			end
-			parts = reduce(hcat, closed[index])
-			found, pass = getindex.(index, 1), getindex.(index, 2)
-			anew  = add.(parts[2, :], dest[found])
-			left  = add.(parts[1, :], src[found])
-			right = add.(parts[3, :], src[found])
-			rest = filter(s -> !(s in pass), 1:length(iter))
-			append!(start, [anew; iter[rest]])
-			iter = filter(!isempty, [left; right])
-		end
-		append!(start, iter)
-	end
-	minimum(start).start
+        for map = maps
+                dest, src, sets = map[1, :], map[2, :], map[3, :]
+                iter = copy(start)
+                resize!(start, 0)
+                for _ = 1:4
+                        !isempty(iter) || break
+                        offset = broadcast((s, c) -> s .- c, permutedims(iter), src)
+                        index = findall(within.(offset, sets))
+                        !isempty(index) || break
+                        parts = reduce(hcat, inrange.(offset, sets)[index])
+                        found, pass = getindex.(index, 1), getindex.(index, 2)
+                        move, keep = dest[found], src[found]
+
+                        remaining = iter[broadcast(s -> !(s in pass), 1:length(iter))]
+                        append!(start, add.(parts[2, :], move), remaining)
+
+                        iter = [add.(parts[1, :], keep)
+                                add.(parts[3, :], keep)]
+                        filter!(!isempty, iter)
+                end
+        end
+        minimum(start).start
 end
 
 function location_b(lines::Vector{String})
-	tables = partitioned(isempty, lines)
-	(seeds, maps) = Iterators.peel(tables)
-	seedling = readstring(readvalues(only(seeds)))
-	reseed = reshape(seedling, (2, length(seedling) ÷ 2))
-	seeded = range.(reseed[1, :], reseed[1, :] .+ reseed[2, :] .- 1)
+        tables = partitioned(isempty, lines)
+        (seeds, maps) = Iterators.peel(tables)
+        seedling = readstring(readvalues(only(seeds)))
+        seeded = range.(seedling[1:2:end], seedling[1:2:end] .+ seedling[2:2:end] .- 1)
 
-	map = [reduce(hcat, readstring.(@view text[2:end])) for text = maps]
-	recurse(seeded, map)
+        map = [reduce(hcat, readstring.(@view text[2:end])) for text = maps]
+        recurse(seeded, map)
 end
 
 function main()
         local file = (length(ARGS) > 0) ? ARGS[1] : "day5/example.txt"
 
         lines = readlines(file)
-	println("Min : ", location_b(lines))
+        println("Min : ", location_b(lines))
 end
 
 main()
